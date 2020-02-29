865e17e331c75d1facab2615ba9ab88b6fa83a02
unknown <charlie@howardgb.com>
2020-02-29 11:40:27 +0000

    Update 3.5.0.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NorthPoint Installer 3.5.0.1
+# NorthPoint Installer 3.5.0.2
 ###### Copyright (c) Charlie Howard 2016-2020 All rights reserved.
 
 When run you will get a form that allows to select what programs you wish to install.
@@ -12,6 +12,10 @@ Changes wallpaper to a selection of HD landscape images.
 Sets the default browser to either Google Chrome or Mozilla Firefox depending on what has been selected, or Firefox if both are selected.
 
 ### Changelog
+
+###### Update 3.5.0.2
+
+Installer now installs Microsoft .NET 3.5 and .NET 4.6.2 as a prerequisite.
 
 ###### Update 3.5.0.1
 
--- computerRepairCentreInstaller.ps1
+++ computerRepairCentreInstaller.ps1
@@ -270,10 +270,10 @@ function computerRepairCentreInstaller {
 		$processRunspace.Open()
 		$processRunspace.SessionStateProxy.SetVariable("syncHash",$syncHash)
 		$psCmd = [powershell]::Create().AddScript({
-				$syncHash.progress.Items.Add("Current version: 3.5.0.1 (29/02/2020)")
+				$syncHash.progress.Items.Add("Current version: 3.5.0.2 (29/02/2020)")
 				$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
 				$syncHash.progress.SelectedIndex = -1;
-				$syncHash.progressBar.Maximum = 4
+				$syncHash.progressBar.Maximum = 6
 				if ($syncHash.crc.Checked) { $syncHash.progressBar.Maximum += 1 }
 				if ($syncHash.mozillaFirefox.Checked) { $syncHash.progressBar.Maximum += 1 }
 				if ($syncHash.mozillaThunderbird.Checked) { $syncHash.progressBar.Maximum += 1 }
@@ -337,6 +337,11 @@ function computerRepairCentreInstaller {
 				$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
 				$syncHash.progress.SelectedIndex = -1;
 				Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+				$syncHash.progress.Items.Add("Installing Microsoft .NET 3.5...")
+				$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
+				$syncHash.progress.SelectedIndex = -1;
+				choco install dotnet3.5 -y
+				$syncHash.progressBar.PerformStep()
 				$syncHash.progress.Items.Add("Installing Microsoft .NET 4.5...")
 				$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
 				$syncHash.progress.SelectedIndex = -1;
@@ -346,6 +351,11 @@ function computerRepairCentreInstaller {
 				$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
 				$syncHash.progress.SelectedIndex = -1;
 				choco install dotnet4.6.1 -y
+				$syncHash.progressBar.PerformStep()
+				$syncHash.progress.Items.Add("Installing Microsoft .NET 4.6.2...")
+				$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
+				$syncHash.progress.SelectedIndex = -1;
+				choco install dotnet4.6.2 -y
 				$syncHash.progressBar.PerformStep()
 				$syncHash.progress.Items.Add("Updating PowerShell...")
 				$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
@@ -855,7 +865,7 @@ function computerRepairCentreInstaller {
 
 	## -- Computer Repair Centre Installer
 
-	$crcInstaller.Text = "Computer Repair Centre Installer 3.5.0.1"
+	$crcInstaller.Text = "Computer Repair Centre Installer 3.5.0.2"
 	$crcInstaller.Name = "crcInstaller"
 	$crcInstaller.DataBindings.DefaultDataSourceUpdateMode = 0
 	$System_Drawing_Size = New-Object System.Drawing.Size
