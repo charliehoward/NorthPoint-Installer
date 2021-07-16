b8ea4ce8c74c032ae7d871bb1f8336e4403c35fb
Charlie Howard <charlie@howardgb.com>
2021-07-16 13:09:43 +0100

    Update 3.10.8.0

--- computerRepairCentreInstaller.ps1
+++ computerRepairCentreInstaller.ps1
@@ -225,6 +225,13 @@ else {
 	$location = 1
 	$locationOpposite = 0
 }
+$computerSystem = (Get-WmiObject -Class:Win32_ComputerSystem)
+if ($computerSystem.Model -like '*EliteBook*'){
+	$eliteBook = 1
+}
+else {
+	$eliteBook = 0
+}
 function computerRepairCentreInstaller {
 	[reflection.assembly]::loadwithpartialname("System.Windows.Forms")
 	[reflection.assembly]::loadwithpartialname("System.Drawing")
@@ -301,7 +308,7 @@ function computerRepairCentreInstaller {
 		$processRunspace.Open()
 		$processRunspace.SessionStateProxy.SetVariable("syncHash",$syncHash)
 		$psCmd = [powershell]::Create().AddScript({
-				$syncHash.progress.Items.Add("Current version: 3.10.7.1 (21/06/2021)")
+				$syncHash.progress.Items.Add("Current version: 3.10.8.0 (16/07/2021)")
 				$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
 				$syncHash.progress.SelectedIndex = -1;
 				$syncHash.progressBar.Maximum = 7
@@ -1157,7 +1164,7 @@ function computerRepairCentreInstaller {
 
 	## -- Computer Repair Centre Installer
 
-	$crcInstaller.Text = "Computer Repair Centre Installer 3.10.7.1"
+	$crcInstaller.Text = "Computer Repair Centre Installer 3.10.8.0"
 	$crcInstaller.Name = "crcInstaller"
 	$crcInstaller.DataBindings.DefaultDataSourceUpdateMode = 0
 	$System_Drawing_Size = New-Object System.Drawing.Size
@@ -1596,7 +1603,7 @@ function computerRepairCentreInstaller {
 	$rebootBox.location = $System_Drawing_Point
 	$rebootBox.DataBindings.DefaultDataSourceUpdateMode = 0
 	$rebootBox.Name = "rebootBox"
-	$rebootBox.Checked = 0
+	$rebootBox.Checked = $eliteBook
 	$rebootBox.Text = "Reboot when install is complete."
 	$crcInstaller.Controls.Add($rebootBox)
 
@@ -1615,7 +1622,7 @@ function computerRepairCentreInstaller {
 	$refurbBox.location = $System_Drawing_Point
 	$refurbBox.DataBindings.DefaultDataSourceUpdateMode = 0
 	$refurbBox.Name = "refurbBox"
-	$refurbBox.Checked = 0
+	$refurbBox.Checked = $eliteBook
 	$refurbBox.Text = "Disable sleep on AC power."
 	$crcInstaller.Controls.Add($refurbBox)
 
