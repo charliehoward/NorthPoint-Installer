b3de759116fe84cdcac2310f8400cdb199e7aca0
Charlie Howard <charlie@howardgb.com>
2021-12-13 16:06:21 +0000

    Update 3.2021.12.13.1

--- computerRepairCentreInstaller.ps1
+++ computerRepairCentreInstaller.ps1
@@ -309,10 +309,10 @@ function computerRepairCentreInstaller {
 		$processRunspace.Open()
 		$processRunspace.SessionStateProxy.SetVariable("syncHash",$syncHash)
 		$psCmd = [powershell]::Create().AddScript({
-				$syncHash.progress.Items.Add("Current version: 3.2021.12.13.0")
+				$syncHash.progress.Items.Add("Current version: 3.2021.12.13.1")
 				$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
 				$syncHash.progress.SelectedIndex = -1;
-				$syncHash.progressBar.Maximum = 12
+				$syncHash.progressBar.Maximum = 11
 				if ($syncHash.crc.Checked) { $syncHash.progressBar.Maximum += 1 }
 				if ($syncHash.mozillaFirefox.Checked) { $syncHash.progressBar.Maximum += 1 }
 				if ($syncHash.mozillaThunderbird.Checked) { $syncHash.progressBar.Maximum += 1 }
@@ -1486,7 +1486,7 @@ function computerRepairCentreInstaller {
 
 	## -- Computer Repair Centre Installer
 
-	$crcInstaller.Text = "Computer Repair Centre Installer 3.2021.12.13.0"
+	$crcInstaller.Text = "Computer Repair Centre Installer 3.2021.12.13.1"
 	$crcInstaller.Name = "crcInstaller"
 	$crcInstaller.DataBindings.DefaultDataSourceUpdateMode = 0
 	$System_Drawing_Size = New-Object System.Drawing.Size
