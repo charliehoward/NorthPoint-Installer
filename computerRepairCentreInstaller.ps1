c09c576858fe82d741bf3a3fa84e2209c9866dd0
Charlie Howard <charlie@howardgb.com>
2022-07-02 17:42:50 +0100

    Update 3.2022.07.02.5

--- computerRepairCentreInstaller.ps1
+++ computerRepairCentreInstaller.ps1
@@ -299,13 +299,13 @@ function computerRepairCentreInstaller {
 		$processRunspace.Open()
 		$processRunspace.SessionStateProxy.SetVariable("syncHash",$syncHash)
 		$psCmd = [powershell]::Create().AddScript({
-				$syncHash.progress.Items.Add("Current version: 3.2022.07.02.4")
+				$syncHash.progress.Items.Add("Current version: 3.2022.07.02.5")
 				$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
 				$syncHash.progress.SelectedIndex = -1;
 				$syncHash.progress.Items.Add("Last updated: 2nd of July 2022")
 				$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
 				$syncHash.progress.SelectedIndex = -1;
-				$syncHash.progressBar.Maximum = 8
+				$syncHash.progressBar.Maximum = 12
 				if ($syncHash.refurbBox.Checked) { $syncHash.progressBar.Maximum += 1 }
 				if ($syncHash.crc.Checked) { $syncHash.progressBar.Maximum += 1 }
 				if ($syncHash.mozillaFirefox.Checked) { $syncHash.progressBar.Maximum += 1 }
@@ -1668,7 +1668,7 @@ function computerRepairCentreInstaller {
 
 	## -- Computer Repair Centre Installer
 
-	$crcInstaller.Text = "Computer Repair Centre Installer 3.2022.07.02.4"
+	$crcInstaller.Text = "Computer Repair Centre Installer 3.2022.07.02.5"
 	$crcInstaller.Name = "crcInstaller"
 	$crcInstaller.DataBindings.DefaultDataSourceUpdateMode = 0
 	$System_Drawing_Size = New-Object System.Drawing.Size
