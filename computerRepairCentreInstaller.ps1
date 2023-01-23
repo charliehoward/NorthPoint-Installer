893409e3bb96d3ef47e12845f49607b516826eb6
Charlie Howard <charlie@howardgb.com>
2023-01-23 19:42:03 +0000

    Update 5.2023.01.23.8

--- computerRepairCentreInstaller.ps1
+++ computerRepairCentreInstaller.ps1
@@ -375,7 +375,7 @@ function computerRepairCentreInstaller {
 		$processRunspace.Open()
 		$processRunspace.SessionStateProxy.SetVariable("syncHash",$syncHash)
 		$psCmd = [powershell]::Create().AddScript({
-				$syncHash.progress.Items.Add("Current version: 5.2023.01.23.7")
+				$syncHash.progress.Items.Add("Current version: 5.2023.01.23.8")
 				$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
 				$syncHash.progress.SelectedIndex = -1;
 				$syncHash.progress.Items.Add("Last updated: 23rd of January 2023")
@@ -693,6 +693,8 @@ function computerRepairCentreInstaller {
 					$syncHash.progress.Items.Add("Moving Kaspersky Standard to the Desktop for manual installation...")
 					$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
 					$syncHash.progress.SelectedIndex = -1;
+					$DesktopPath = [Environment]::GetFolderPath("Desktop")
+					Move-Item -Path "C:\Computer Repair Centre\kaspersky.exe" -Destination "$DesktopPath\kaspersky.exe"
 					$syncHash.progress.Items.Add("Please run kaspersky.exe after installation.")
 					$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
 					$syncHash.progress.SelectedIndex = -1;
@@ -1349,7 +1351,7 @@ function computerRepairCentreInstaller {
 
 	## -- Computer Repair Centre Installer
 
-	$crcInstaller.Text = "Computer Repair Centre Installer 5.2023.01.23.7"
+	$crcInstaller.Text = "Computer Repair Centre Installer 5.2023.01.23.8"
 	$crcInstaller.Name = "crcInstaller"
 	$crcInstaller.DataBindings.DefaultDataSourceUpdateMode = 0
 	$System_Drawing_Size = New-Object System.Drawing.Size
