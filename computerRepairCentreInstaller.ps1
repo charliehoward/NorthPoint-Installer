0c38c5c740df7dcf98974c65632d80a38c240354
Charlie Howard <charlie@howardgb.com>
2023-01-30 16:52:46 +0000

    Update 5.2023.01.30.3

--- computerRepairCentreInstaller.ps1
+++ computerRepairCentreInstaller.ps1
@@ -369,7 +369,7 @@ function computerRepairCentreInstaller {
 		$processRunspace.Open()
 		$processRunspace.SessionStateProxy.SetVariable("syncHash",$syncHash)
 		$psCmd = [powershell]::Create().AddScript({
-				$syncHash.progress.Items.Add("Current version: 5.2023.01.30.2")
+				$syncHash.progress.Items.Add("Current version: 5.2023.01.30.3")
 				$syncHash.progress.SelectedIndex = $syncHash.progress.Items.Count - 1;
 				$syncHash.progress.SelectedIndex = -1;
 				$syncHash.progress.Items.Add("Last updated: 30th of January 2023")
@@ -1324,7 +1324,7 @@ function computerRepairCentreInstaller {
 
 	## -- Computer Repair Centre Installer
 
-	$crcInstaller.Text = "Computer Repair Centre Installer 5.2023.01.30.2"
+	$crcInstaller.Text = "Computer Repair Centre Installer 5.2023.01.30.3"
 	$crcInstaller.Name = "crcInstaller"
 	$crcInstaller.DataBindings.DefaultDataSourceUpdateMode = 0
 	$System_Drawing_Size = New-Object System.Drawing.Size
